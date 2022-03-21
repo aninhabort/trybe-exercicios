093aa53c704fc20e494b9f22ad70a72ad7ae9d03
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2022-03-20 23:35:37 -0300

    exercicio dia 5.4

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/scripts.js
@@ -0,0 +1,8 @@
+addEventListener('click', function darkModeOn() {
+    var b = document.body;
+    if (b.backgrounColor = 'white') {
+        b.backgrounColor = 'black';
+    } else {
+        b.backgrounColor = 'white';
+    }
+})
