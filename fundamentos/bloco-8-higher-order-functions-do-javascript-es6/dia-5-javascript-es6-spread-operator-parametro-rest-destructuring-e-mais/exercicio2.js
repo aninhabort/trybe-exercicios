3e4e1dac9f3a46ee5ec398ca4588f37a308786c4
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2022-04-13 21:01:22 -0300

    Exercicio do dia 8.5

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio2.js
@@ -0,0 +1,3 @@
+// escreva sum abaixo
+const sum = (...num) => num.reduce((num1, num2) => num1 + num2);
+console.log(sum(5, 9));
---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio5.js
@@ -0,0 +1,5 @@
+const myList = [1, 2, 3];
+
+// escreva swap abaixo
+const [first, second, third] = myList;
+console.log(third, second, first);
