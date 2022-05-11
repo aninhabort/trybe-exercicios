ba700f153ab66bf324dd93cc4a24eb8cc8d406f8
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2022-05-10 21:53:42 -0300

    Atualizando infos

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Card.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Card.js
@@ -7,12 +7,13 @@ class Card extends React.Component {
     render() {
         return (
             <div className='container'>
-                {data.map(({ name, type, averageWeight, image }) => (
+                {data.map(({ name, type, averageWeight, image, moreInfo }) => (
                     <div className="card">
                         <div className="text">
                             <p>{name}</p>
                             <p>{type}</p>
                             <p>{averageWeight.value}{averageWeight.measurementUnit}</p>
+                            <a href={moreInfo} target="_blank" rel="noreferrer">More Info</a>
                         </div>
                         <img src={image} alt={name}></img>
                     </div>
