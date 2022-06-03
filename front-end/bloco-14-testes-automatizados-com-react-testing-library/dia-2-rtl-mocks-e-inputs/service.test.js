c5490e32f1534e382b9fec3295e8480a902760f6
Ana Carolina Magalhaes <carol2015bortolini@gmail.com>
2022-06-02 21:23:41 -0300

    continuacao do exercicio do dia 3

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service.test.js
@@ -1,4 +1,4 @@
-const { test, expect } = require('@jest/globals')
+const { test, expect } = require('@jest/globals');
 const service = require('./service');
 
 test('teste se a função foi chamada, se seu retorno for 10 e quantas vezes foi chamado', () => {
@@ -7,4 +7,12 @@ test('teste se a função foi chamada, se seu retorno for 10 e quantas vezes foi
     expect(service.randomNum()).toBe(10);
     expect(service.randomNum).toHaveBeenCalled();
     expect(service.randomNum).toHaveBeenCalledWith();
+})
+
+test('teste se ao receber dois parametros e retornar a divisao do primeiro pelo segundo, ela deve ocorrer uma vez', () => {
+    service.randomNum = jest.fn().mockImplementation((a, b) => a / b);
+
+    expect(service.randomNum(10, 2)).toBe(5);
+    expect(service.randomNum).toHaveBeenCalled();
+    expect(service.randomNum).toHaveBeenCalledWith(10, 2);
 })
